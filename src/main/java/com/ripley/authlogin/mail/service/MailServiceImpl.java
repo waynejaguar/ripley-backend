7c991fd07380d53d52350f2b4bc7520e1be1a151
jonathan <joanathan.meneses@walmart.com>
2021-02-23 09:07:52 -0300

    [Jonathan] remove comments lines

--- src/main/java/com/ripley/authlogin/mail/service/MailServiceImpl.java
+++ src/main/java/com/ripley/authlogin/mail/service/MailServiceImpl.java
@@ -23,7 +23,6 @@ public class MailServiceImpl implements MailService{
     Email to = new Email(emailTo);
     Content content = new Content("text/plain", "este es un correo de banco ripley su contrasena es:" + password);
     Mail mail = new Mail(from, subject, to, content);
-
     SendGrid sg = new SendGrid(System.getenv("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"));
     Request request = new Request();
     try {
@@ -31,10 +30,6 @@ public class MailServiceImpl implements MailService{
       request.setEndpoint("mail/send");
       request.setBody(mail.build());
       Response response = sg.api(request);
-      //TODO: quitar syso
-      System.out.println(response.getStatusCode());
-      System.out.println(response.getBody());
-      System.out.println(response.getHeaders());
     } catch (IOException ex) {
       throw ex;
     }
